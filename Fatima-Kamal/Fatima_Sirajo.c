2c89c9a2d0c20ef7bca23b85c1ff7107a2a01263
Abubakar Abdullahi Dauda <94997950+abduldauda@users.noreply.github.com>
2021-11-28 14:20:39 +0100

    Add files via upload

---
+++ Fatima-Kamal/Fatima_Sirajo.c
@@ -0,0 +1,17 @@
+#<-----------Fatima Sirajo C programming Project------------>
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("BIOINFORMATICS");
+	printf("NAME: Fatima Sirajo");
+	printf("EMAIL: sirajofatima@gmail.com");
+	printf("SLACK ID: Fatima sirajo");
+	printf("GITHUB ID:fatimasiraj");
+	printf("AREA OF INTREST: Compututional Biology");
+	getch();
+
+
+}
---
+++ Fatima-Kamal/Kamal_Musa.c
@@ -0,0 +1,17 @@
+#<-----------Kamal Musa C programming Project------------>
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("BIOINFORMATICS");
+	printf("NAME: Kamal Musa");
+	printf("EMAIL: kamaluddeenmusa5@gmail.com");
+	printf("SLACK ID: Kamal S Musa");
+	printf("GITHUB ID:Khamalmz");
+	printf("AREA OF INTREST: DNA Sequensing");
+	getch();
+
+
+}

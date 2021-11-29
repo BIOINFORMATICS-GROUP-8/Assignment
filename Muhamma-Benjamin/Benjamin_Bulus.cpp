90634f6e2ec4fbc55ae1d1597bca316cd004a27a
Abubakar Abdullahi Dauda <94997950+abduldauda@users.noreply.github.com>
2021-11-30 00:21:12 +0100

    Add files via upload

---
+++ Muhamma-Benjamin/Benjamin_Bulus.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name,email,github,slackid,programming,areaofintrest;
+	cout<<"Please Enter Your Name"<<endl;
+	cin>>name;
+	cout<<"Please Enter Your Email"<<endl;
+	cin>>email;
+	cout<<"Please Enter Your Github ID"<<endl;
+	cin>>github;
+	cout<<"Please Enter Your Slack ID"<<endl;
+	cin>>slackid;
+	cout<<"Please Enter Your Area of Intrest"<<endl;
+	cin>>areaofintrest;
+	cout<<Name : "<<name<<endl;
+	cout<<Email : "<<email<<endl;
+	cout<<Github ID : "<<github<<endl;
+	cout<<Slack ID : "<<slackid<<endl;
+	cout<<Area of Intrest : "<<areaofintrest<<endl;
+
+
+
+}

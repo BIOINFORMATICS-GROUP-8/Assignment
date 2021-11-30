a27c35774ffb8832e7b88395bab63dc8e44cbf50
Abubakar Abdullahi Dauda <94997950+abduldauda@users.noreply.github.com>
2021-11-30 01:52:28 +0100

    Add files via upload

---
+++ Muntaka-Abdulhamid/Abdulhamid_Muntaka.rb
@@ -0,0 +1,9 @@
+puts "Abdulhamid Magaji"
+puts "abdulhamidmagaji148@gmail.com"
+puts "Magaji148"
+puts "Field of Interest: Algorithm "
+
+name = gets
+email = gets
+github = gets
+field = gets
---
+++ Muntaka-Abdulhamid/Muntaka_Abdulhamid.rb
@@ -0,0 +1,9 @@
+puts "Muntaka Abdulrahman"
+puts "abdulrahamanmuntaka@gmail.com"
+puts "Github: @67099287"
+puts "Field of Interest: Algorithm "
+
+name = gets
+email = gets
+github = gets
+field = gets
